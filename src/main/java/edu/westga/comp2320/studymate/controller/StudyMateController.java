2354bfb2cf79455d84387b389570d31a78e2168c
Raphael Rao <rrao1@my.westga.edu>
2026-04-16 18:14:03 -0400

    Refactor starter classes and organize packages

--- src/main/java/edu/westga/comp2320/studymate/controller/StudyMateController.java
+++ src/main/java/edu/westga/comp2320/studymate/controller/StudyMateController.java
@@ -1,9 +1,9 @@
-package edu.westga.comp2320.studymate;
+package edu.westga.comp2320.studymate.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StudyMateController {
     @FXML
     private Label welcomeText;
 
